33664bfa688ade5746d12d78cf2765825b214886
YangZhuoxun725 <yangzhuoxun1234@gmail.com>
2025-07-04 14:54:31 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ with st.sidebar:
 
     llm = 'a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5'
     weight = st.number_input(
-        "Input your weight: ", value=None, placeholder="Ur fat")
+        "Input your weight: ", value=None, placeholder="Ur fat"
     )
 
 # Store LLM generated responses
